fc712bfc4db40e1180c2373ff2e534547608c476
Sylwester Sojka <sojka.sylwester.dev@gmail.com>
2024-09-01 13:20:14 +0200

    Last update before the code review

--- app/streamlit_transaction_section.py
+++ app/streamlit_transaction_section.py
@@ -33,37 +33,40 @@ def get_timeline():
 
 
 def render_transaction_section():
-        
-        csv_handler = CSVHandler()
-        df_tr = None
-        tr_tab1, tr_tab2, tr_tab3  = st.tabs(["Load CSV", "Details of transactions", "Summary"])
-        
+    csv_handler = CSVHandler()
+    df_tr = None
+    tr_tab1, tr_tab2, tr_tab3 = st.tabs(["Load CSV", "Details of transactions", "Summary"])
 
-        with tr_tab1:
-            st.title('Load CSV to DB')
-            print(f'1/2::: trying to upload a file')
-            uploaded_file = st.file_uploader("Choose CSV file", type="csv")
-            print(f'Uploaded file is: {uploaded_file}')
-            print(f'2/2::: Code after file uploading')
-            
-            all_transactions = get_all_transactions()
-            print(all_transactions)
-            
+    with tr_tab1:
+        st.title('Load CSV to DB')
+        print(f'1/2::: trying to upload a file')
+        uploaded_file = st.file_uploader("Choose CSV file", type="csv")
+        print(f'Uploaded file is: {uploaded_file}')
+        print(f'2/2::: Code after file uploading')
 
-            if uploaded_file is not None:
-                response = add_csv_to_db(uploaded_file)
-                if response is not None:
-                    st.success(response)
+        all_transactions = get_all_transactions()
+        print(all_transactions)
 
-            elif len(all_transactions) > 0:
-                pass
+        if uploaded_file is not None:
+            response = add_csv_to_db(uploaded_file)
+            if response is not None:
+                st.success(response)
+                all_transactions = get_all_transactions()
+                if all_transactions:
+                    df_tr = pd.DataFrame(all_transactions)
+                    print("DataFrame created after file upload.")
 
-            else:
-                st.info("Please upload a CSV file")
+        elif len(all_transactions) > 0:
+            df_tr = pd.DataFrame(all_transactions)
 
+        else:
+            st.info("Please upload a CSV file")
+
+        if df_tr is not None and not df_tr.empty:
+            df_tr = csv_handler.remove_dupl(df=df_tr)
             time_lines = get_timeline()
             timelines = list(time_lines.values())
-            
+
             timeline_selection = st.multiselect(
                 "Select timeline to display:",
                 timelines,
@@ -72,26 +75,17 @@ def render_transaction_section():
 
             print(f'timeline_selection is : {timeline_selection}')
 
-            if all_transactions:
-                df_tr = pd.DataFrame(all_transactions)
-
-                if not timeline_selection:
-
-                    if not df_tr.empty and 'receiver' in df_tr.columns:
-                        df_tr = csv_handler.remove_dupl(df=df_tr)
-                        st.dataframe(df_tr)
-                    else:
-                        st.warning("DataFrame is empty or 'receiver' column not found.")
-
-                else:
-                    print(f'Timeline selection is: {timeline_selection}')
-                    filtered_df = df_tr[df_tr['exec_month'].isin(timeline_selection)]
-
-                    st.dataframe(filtered_df)
-
+            if timeline_selection:
+                print(f'Timeline selection is: {timeline_selection}')
+                filtered_df = df_tr[df_tr['exec_month'].isin(timeline_selection)]
+                st.dataframe(filtered_df)
             else:
-                st.info("No transactions available in the database.")
+                st.warning("No timeline selected. Showing all data.")
+                st.dataframe(df_tr)
+        else:
+            st.info("No transactions available in the database.")
 
+    if df_tr is not None and not df_tr.empty:
         with tr_tab2:
             met_col1, met_col2, met_col3 = st.columns(3)
             tr_col1, tr_col2 = st.columns(2)
@@ -102,19 +96,15 @@ def render_transaction_section():
                 key=f"timeline_selection_details"
             )
 
-            
+            filtered_df_summary = df_tr[df_tr['exec_month'].isin(timeline_selection_details)]
 
             with tr_col1:
-
                 st.title(f'Expenses')
-
-                filtered_df_summary = df_tr[df_tr['exec_month'].isin(timeline_selection_details)]
-
                 grouped_df = filtered_df_summary.groupby('receiver')['amount'].sum().reset_index()
                 grouped_df_exp = grouped_df[grouped_df['amount'] < 0]
                 grouped_df_exp.columns = ['Reciever', 'Value']
 
-                #Reseting index
+                # Reseting index
                 grouped_df_exp.reset_index(drop=True, inplace=True)
                 grouped_df_exp.index = grouped_df_exp.index + 1
                 grouped_df_exp.index.name = "Row Number"
@@ -122,13 +112,12 @@ def render_transaction_section():
                 st.dataframe(grouped_df_exp, use_container_width=True)
 
             with tr_col2:
-
                 st.title(f'Income')
                 grouped_df = filtered_df_summary.groupby('receiver')['amount'].sum().reset_index()
                 grouped_df_inc = grouped_df[grouped_df['amount'] > 0]
                 grouped_df_inc.columns = ['Reciever', 'Value']
 
-                #Reseting index
+                # Reseting index
                 grouped_df_inc.reset_index(drop=True, inplace=True)
                 grouped_df_inc.index = grouped_df_inc.index + 1
                 grouped_df_inc.index.name = "Row Number"
@@ -137,14 +126,11 @@ def render_transaction_section():
 
             with met_col1:
                 if timeline_selection_details:
-                    
                     sum_of_exp = round(grouped_df_exp['Value'].sum(), 2)
                     st.metric(label='Expenses', value=sum_of_exp)
-                
 
             with met_col2:
                 if timeline_selection_details:
-                    
                     sum_of_inc = round(grouped_df_inc['Value'].sum(), 2)
                     st.metric(label='Income', value=sum_of_inc)
 
@@ -152,15 +138,14 @@ def render_transaction_section():
                 st.write("Savings")
 
             add_left, add_middle, add_right = st.columns(3, vertical_alignment="bottom")
-            
+
             rule_key = add_left.text_input("New Value:")
             rule_val = add_middle.text_input("Text to replace (contain New Value):")
 
             if add_right.button("Add Rule", use_container_width=True):
-                print(f"Pressing add rule button adding folowing key:{rule_key} and val:{rule_val}")
+                print(f"Pressing add rule button adding following key:{rule_key} and val:{rule_val}")
                 csv_handler.add_rule(rule_key=rule_key, rule_value=rule_val)
                 st.rerun()
 
-
-            with tr_tab3:
-                pass
+    with tr_tab3:
+        pass
